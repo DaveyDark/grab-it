f320cb5bac5e9a4bf89b04c0721f4c7ab8c8d037
DaveyDark <devesh97531@gmail.com>
2024-01-20 04:09:58 +0530

    hooks

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/contact", "/api/hook"],
+  ignoredRoutes: ["/api/hook", "/api/chatgpt", "/api/(.*)"],
 });
 
 export const config = {
