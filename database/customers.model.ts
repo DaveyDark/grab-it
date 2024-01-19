648faaad1d0cdd3e7ceb623071a2d2623dcd5db7
DaveyDark <devesh97531@gmail.com>
2024-01-20 02:44:00 +0530

    Database Models

--- database/customers.model.ts
+++ database/customers.model.ts
@@ -1,23 +1,21 @@
 import { Schema } from "mongoose";
 
 export interface ICustomers extends Document {
-  CId: Schema.Types.ObjectId;
-  Name: string;  
+  Name: string;
   Address: string;
-  
+
   Pincode: number;
   City: string;
   State: string;
   Latitude: number;
-  Longitude: number; 
+  Longitude: number;
 }
-const CustomersSchema: Schema = new Schema({ 
-    CId : { type: Schema.Types.ObjectId, required: true },
-    Name: { type: String, required: true },
-    Address: { type: String, required: true },
-    Pincode: { type: Number, required: true },
-    City: { type: String, required: true },
-    State: { type: String, required: true },
-    Latitude: { type: Number, required: true },
-    Longitude: { type: Number, required: true },
-    });
+const CustomersSchema: Schema = new Schema({
+  Name: { type: String, required: true },
+  Address: { type: String, required: true },
+  Pincode: { type: Number, required: true },
+  City: { type: String, required: true },
+  State: { type: String, required: true },
+  Latitude: { type: Number, required: true },
+  Longitude: { type: Number, required: true },
+});
