46cdfec7c5068f137b0a2990595609dba557f22b
DaveyDark <devesh97531@gmail.com>
2024-01-20 04:05:36 +0530

    hooks

--- lib/actions/seller.action.ts
+++ lib/actions/seller.action.ts
@@ -1,5 +1,6 @@
 import Seller from "@/database/seller.model";
 import { connectToDatabase } from "../mongoose";
+import { revalidatePath } from "next/cache";
 
 export async function getIdFromClerkId(clerkId: string) {
   try {
@@ -13,16 +14,51 @@ export async function getIdFromClerkId(clerkId: string) {
     throw new Error(err);
   }
 }
-
-export async function updateSellerProfile(params: any) {
+export async function createSeller(userData: any) {
   try {
     await connectToDatabase();
+    const newSeller = await Seller.create(userData);
+    return JSON.stringify(newSeller.toJSON());
+  } catch (err: any) {
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
+  }
+}
 
-    await Seller.findOneAndUpdate({ _id: params.id }, params, {
+export async function updateSeller(userData: {
+  clerkId: string;
+  updateData: any;
+  path: string;
+}) {
+  try {
+    await connectToDatabase();
+    const { clerkId, updateData, path } = userData;
+    const user = await Seller.findOneAndUpdate({ clerkId }, updateData, {
       new: true,
     });
+    revalidatePath(path);
   } catch (err: any) {
-    console.error(`Error updating seller: ${err}`);
-    throw new Error(err);
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
+  }
+}
+
+export async function deleteSeller(id: string) {
+  try {
+    await connectToDatabase();
+    const user = await Seller.findOne({ clerkId: id });
+    if (!user) {
+      throw new Error("Seller not found");
+    }
+    //TODO:
+    // const userQuestions = await Question.find({ author: user._id }).distinct(
+    //   "_id",
+    // );
+    // await Question.deleteMany({ author: user._id });
+    const deletedSeller = await Seller.findByIdAndDelete(user._id);
+    return deletedSeller;
+  } catch (err: any) {
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
   }
 }
