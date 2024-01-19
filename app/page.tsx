5feb7a3042b5e9586954db2a4dc85e60b0095332
DaveyDark <devesh97531@gmail.com>
2024-01-20 03:34:13 +0530

    Apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-import Navbar from '@/components/navbar/Navbar'
-import React from 'react'
+import Navbar from "@/components/navbar/Navbar";
+import React from "react";
 
 const page = () => {
   return (
     <section>
-
       <Navbar />
 
-      <div className='flex items-center flex-col bg-gray-100 '>
-        <p className='text-6xl mt-16 mb-5 font-bold'><span className='text-8xl font-bold text-[#FFB700]'>Grab</span> before it's gone...</p>
-        <p className='text-4xl font-semibold text-[#A6A6A6] mb-5'>Unbeatable<span className='text-[#FFB700]'> Discounts</span> on exciting ranges of products</p>
+      <div className="flex items-center flex-col bg-gray-100 ">
+        <p className="text-6xl mt-16 mb-5 font-bold">
+          <span className="text-8xl font-bold text-[#FFB700]">Grab</span> before
+          it&apos;s gone...
+        </p>
+        <p className="text-4xl font-semibold text-[#A6A6A6] mb-5">
+          Unbeatable<span className="text-[#FFB700]"> Discounts</span> on
+          exciting ranges of products
+        </p>
       </div>
-
     </section>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
