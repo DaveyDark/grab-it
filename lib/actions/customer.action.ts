24c8176d507ef58140406ff568c4032794805f66
DaveyDark <devesh97531@gmail.com>
2024-01-20 04:30:03 +0530

    hooks

---
+++ lib/actions/customer.action.ts
@@ -0,0 +1,48 @@
+import Seller from "@/database/seller.model";
+import { connectToDatabase } from "../mongoose";
+import Customer from "@/database/customer.model";
+import { revalidatePath } from "next/cache";
+
+export async function createUser(userData: any) {
+  try {
+    await connectToDatabase();
+    const newUser = await Customer.create(userData);
+    return JSON.stringify(newUser.toJSON());
+  } catch (err: any) {
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
+  }
+}
+
+export async function updateUser(userData: {
+  clerkId: string;
+  updateData: any;
+  path: string;
+}) {
+  try {
+    await connectToDatabase();
+    const { clerkId, updateData, path } = userData;
+    const user = await Customer.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+  } catch (err: any) {
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
+  }
+}
+
+export async function deleteUser(id: string) {
+  try {
+    await connectToDatabase();
+    const user = await Customer.findOne({ clerkId: id });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const deletedUser = await Customer.findByIdAndDelete(user._id);
+    return deletedUser;
+  } catch (err: any) {
+    console.error(`Error getting user: ${err.message}`);
+    throw err;
+  }
+}
