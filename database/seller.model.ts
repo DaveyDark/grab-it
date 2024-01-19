d3d246bbe344909d901ac83d750b9fb2cbc06460
DaveyDark <devesh97531@gmail.com>
2024-01-19 22:31:19 +0530

    Seller database model

---
+++ database/seller.model.ts
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+
+export interface ISeller extends Document {
+  name: string;
+  address: string;
+  bussinessName: string;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+const SellerSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  bussinessName: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+});
