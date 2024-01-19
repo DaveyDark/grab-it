b0af951774ef4526cf52053aca159860de706e22
its-me-ojas <crestfallenwizard@gmail.com>
2024-01-20 00:32:31 +0530

    clerk added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
     <div className='flex flex-row items-center h-36 bg-green-600'>
+      {/* <Image /> */}
 
+      <Input />
+
+      <Button variant="outline">My Cart</Button>
+      <UserButton />
     </div>
   )
 }
