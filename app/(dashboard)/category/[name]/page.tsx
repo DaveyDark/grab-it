b5740f0efb2ef1c1ce5fd9943a925a142fb3f262
its-me-ojas <crestfallenwizard@gmail.com>
2024-01-20 12:43:09 +0530

    logo changed && category page updated

--- app/(dashboard)/category/[name]/page.tsx
+++ app/(dashboard)/category/[name]/page.tsx
@@ -1,29 +1,29 @@
-import Navbar from '@/components/navbar/Navbar'
-import SmallNavbar from '@/components/navbar/smallNavbar/page'
-import ProductCard from '@/components/productCard/page'
-import React from 'react'
+import Navbar from '@/components/navbar/Navbar';
+import SmallNavbar from '@/components/smallNavbar/page';
+import ProductCard from '@/components/productCard/page'; // Corrected import path
+import React from 'react';
+import { productDetails } from '@/constants/category'; // Assuming this is the correct import path
 
-const Products = ({ params }: any) => {
+const Products = () => {
   return (
     <div className='bg-[#F6FFF6] min-h-screen'>
       <Navbar />
       <SmallNavbar />
       <div className='flex flex-row flex-wrap px-32 pt-20 justify-center'>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {productDetails.map((product, index) => (
+          <ProductCard
+            key={index} // Added key attribute for list rendering
+            img={product.img}
+            name={product.name}
+            weight={product.weight}
+            expiring={product.expiring}
+            originalPrice={product.originalPrice}
+            discountedPrice={product.discountedPrice}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
